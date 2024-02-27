09f200cf31c1652d7a93da2e33660312fef4088c
Azutech <azunna.onugha@gmail.com>
2024-02-28 00:59:05 +0100

    Chore(): Server SetUp created for Task

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import http from 'http';
+import {config} from 'dotenv';
+import { logger } from './middlewares/logger';
+
+// import database from './connections/database';
+import { server } from './server';
+
+
+config()
+
+import { PORT } from './utils/config';
+
+const httpServer = http.createServer(server);
+
+const app = async () => {
+	try {
+		// database().catch((err) => console.error(err));
+		httpServer.listen(PORT, () => {
+			logger.info(
+				`Transport-Dek is listening at http://localhost:${PORT} 🚀🚀`,
+			);
+		});
+	} catch (err) {
+		console.error(err);
+	}
+};
+
+app();
+
+export default httpServer;
