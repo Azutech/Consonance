10ffb29a5731f38106cd03422fe79a1db5f4c821
Azutech <azunna.onugha@gmail.com>
2024-02-28 02:04:51 +0100

    Feature(): Register Endpoints Register endpoints created

---
+++ src/controllers/login.ts
@@ -0,0 +1,10 @@
+import {Request, Response} from 'express'
+import { StatusCodes } from 'http-status-codes'
+import { User } from '../models/user'
+import { hashPassword } from '../utils/hashpassword'
+import { validatePassword } from '../utils/validatePassword'
+
+
+export const login = async (req:Request, res: Response) => {
+    
+}
